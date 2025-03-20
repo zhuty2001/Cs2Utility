594d15741c0a295cc6615be05459e035c1d82189
zhuty2001 <your-email@example.com>
2025-03-20 07:08:56 -0500

    Fix 404 issue with client-side routing

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/Cs2Utility/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
